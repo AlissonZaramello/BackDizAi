a52df0e27428ed537fdc564c82a0672f17a575ab
Alisson Zaramello <97170356+AlissonZaramello@users.noreply.github.com>
2025-11-01 02:02:24 -0300

    Nova rota para criação das tabelas

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "ok", "message": "Servidor placeholder funcionando!"}
+
+@app.get("/create-tables")
+def create_tables():
+    Base.metadata.create_all(engine)
+    return {"status": "Tabelas criadas com sucesso"}
