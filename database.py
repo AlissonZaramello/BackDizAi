bcd855067638b7e3f91b2ef6e98991ae8e9620b5
Alisson Zaramello <97170356+AlissonZaramello@users.noreply.github.com>
2025-11-14 20:08:17 -0300

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = "postgresql+psycopg://banco_dizai_user:qhQ2gyuq9S1rVnwNcpCH6YgPawmB5nR7@dpg-d42osver433s73drgib0-a/banco_dizai"
+DATABASE_URL = "postgresql://postgres:rQbsUkDlJVRzUSdQbHpXaLFZGSSgOQOH@hopper.proxy.rlwy.net:27942/railway"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
