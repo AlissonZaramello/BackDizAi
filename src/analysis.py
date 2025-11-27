c0c26c58cc2bf9e7343d354462dc501d6ef17bef
Jv Secate <55952162+JvSecate@users.noreply.github.com>
2025-11-27 01:33:08 -0300

    Análise de sentimento: Nota ponderada

--- src/analysis.py
+++ src/analysis.py
@@ -1,15 +1,19 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import numpy as np
 
 model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer, return_all_scores=True)
 
 def feedback_analysis(review: str):
-    result = classifier(review)[0]
-    label = result['label']
-    confidence = result['score']     
-    score = int(label[0])           
+    scores = classifier(review)[0] 
 
-    return score, confidence
+    probs = np.array([item["score"] for item in scores])
+    stars = np.array([1, 2, 3, 4, 5])
+    weighted_score = float(np.dot(probs, stars))
+    final_score = int(round(weighted_score))
+    confidence = float(probs[final_score - 1])
+
+    return final_score, confidence
